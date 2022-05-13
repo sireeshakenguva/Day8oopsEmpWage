854d2d4d1ea2e646fc81d2635769294adb73a6ac
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-13 18:32:16 +0530

    UsingSwitchCase

--- OopsEmpWage/src/com/bridgelabz/oops/EmployeeWageProblem.java
+++ OopsEmpWage/src/com/bridgelabz/oops/EmployeeWageProblem.java
@@ -1,32 +1,30 @@
 package com.bridgelabz.oops;
 
 public class EmployeeWageProblem {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-	System.out.println("Welcome to EmployeeWageProblem");
-	
-	int IS_FULL_TIME = 1;
- 	int IS_PART_TIME = 2;
-	int EMP_RATE_PER_HOUR = 20;
-	int empHrs = 0;
-	int empWage = 0;
-	double empCheck = Math.floor(Math.random()*10)%3;
-	
-	if (empCheck == IS_FULL_TIME) {
-		System.out.println("Employee is Present");
-		empHrs = 8;
-	}
-	
-	else if (empCheck == IS_PART_TIME) {
-		System.out.println("Employee is Present");
-		empHrs = 4;
-	}
+		System.out.println("Welcome to Employee Wage Problem");
+
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
 		
-	else {
-		System.out.println("Employee is Absent");
-		empHrs = 0;
-	}
-	
-	empWage = empHrs*EMP_RATE_PER_HOUR;
-	System.out.println("Emp Wage:" + empWage);
+		switch(empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+				
+		}
+		
+		empWage = empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
 }
 }

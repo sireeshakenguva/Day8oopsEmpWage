8158bfc26b906b9608adcbb05e2d03e7641bbf00
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-13 18:18:26 +0530

    Employee Attendance

--- OopsEmpWage/src/com/bridgelabz/oops/EmployeeWageProblem.java
+++ OopsEmpWage/src/com/bridgelabz/oops/EmployeeWageProblem.java
@@ -3,5 +3,14 @@ package com.bridgelabz.oops;
 public class EmployeeWageProblem {
 	public static void main(String[] args) {
 	System.out.println("Welcome to EmployeeWageProblem");
+	
+	int IS_FULL_TIME = 1;
+	double empCheck = Math.floor(Math.random()*10)%2;
+	if (empCheck == IS_FULL_TIME) {
+		System.out.println("Employee is present");
+	}
+	else {
+		System.out.println("Employee is Absent");
+	}
 }
 }
